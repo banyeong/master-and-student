da624cb5d5516b737a295e04269b2d90eb99a95f
정수빈 <banyeong321@gmail.com>
2020-07-09 15:51:49 +0900

    first commit
    첫번째

---
+++ Master_AND_Student/Assets/C#/StudentStat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudentStat : MonoBehaviour
+{
+    public static StudentStat instance;
+
+    public int MslStr;//근력
+    public int currentMslStr;
+
+    public int MoralStr;//도력
+    public int currentMoralStr;
+
+    public int Wealth;//재력
+    public int currentWealth;
+
+    public int Favorability;//호감도
+    public int cureentFavorability;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
